6f71671e5d10e683aab989c0f1498a60ce77b180
yurisokolovicz <yurisokolovicz@gmail.com>
2023-04-11 16:23:22 -0300

    Dispatching Actions From Inside Components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,20 @@
 import styles from './Counter.module.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 // useSelector is a hook that allows us to extract data from the Redux store state. This hook is a function that takes a function as an argument. This function will receive the Redux store state as an argument and should return the data we want to extract from the store state. The useSelector hook will then return this extracted data. This hook will automatically re-run whenever the store state changes and will return the latest extracted data.
 
 const Counter = () => {
+    const dispatch = useDispatch();
+
     // When we use useSelector react-redux will automatically subscribe to the Redux store and will re-run the function we pass to useSelector whenever the store state changes. This means that we don't need to manually subscribe to the store and we don't need to manually re-run the function we pass to useSelector whenever the store state changes. This is all handled by react-redux for us.
     const counter = useSelector(state => state.counter);
+
+    const incrementHandler = () => {
+        dispatch({ type: 'increment' }); // increment is the action type from src/store/index.js
+    };
+
+    const decrementHandler = () => {
+        dispatch({ type: 'decrement' }); // decrement is the action type from src/store/index.js
+    };
 
     const toggleCounterHandler = () => {};
 
@@ -12,6 +22,10 @@ const Counter = () => {
         <main className={styles.counter}>
             <h1>Redux Counter</h1>
             <div className={styles.value}>{counter}</div>
+            <div>
+                <button onClick={decrementHandler}>Decrement </button>
+                <button onClick={incrementHandler}>Increment </button>
+            </div>
             <button onClick={toggleCounterHandler}>Toggle Counter</button>
         </main>
     );

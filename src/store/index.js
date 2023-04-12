36884493055dcd8375da03588c6dc66df1b546f4
yurisokolovicz <yurisokolovicz@gmail.com>
2023-04-12 15:59:21 -0300

    Working with Multiple Slices - Authentication

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 // configureStore is a function from Redux Toolkit that combines the createStore function from Redux with the applyMiddleware function from Redux.
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 const counterSlice = createSlice({
     name: 'counter',
-    initialState: initialState,
+    initialState: initialCounterState,
     reducers: {
         increment(state) {
-            state.counter++; // Here we can mutate the state directly because we are using the createSlice function from Redux Toolkit. This is not possible with the createStore function from Redux.
+            state.counter++;
         },
         decrement(state) {
             state.counter--;
@@ -22,10 +22,26 @@ const counterSlice = createSlice({
     }
 });
 
+const initialAuthState = { isAuthenticated: false };
+
+const authSlice = createSlice({
+    name: 'authentication',
+    initialState: initialAuthState,
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
+        }
+    }
+});
+
 const store = configureStore({
-    reducer: counterSlice.reducer
+    reducer: { counter: counterSlice.reducer, auth: authSlice.reducer }
 });
 // CounterActions is an object that contains all the action creators for the counter slice. It includes the increment, decrement, increase, and toggleCounter action creators.
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
